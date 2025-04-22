2b442ccb106e3822eee988679681a209fb7817f9
kinguzbek200828 <kinguzbek200828@gmail.com>
2025-04-22 09:42:06 +0000

    vercel.json addded

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
+
+@app.route("/<part>")
+def returndata(part):
+    with open(f"speaking_{part}.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        return data, 200
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
