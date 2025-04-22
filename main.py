90ca2e226c2c3b779ed52f33fb6f5173a3078a09
kinguzbek200828 <kinguzbek200828@gmail.com>
2025-04-22 10:34:31 +0000

    Translation func

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data import client, translator
 import json
 
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -20,9 +20,13 @@ def returndata(part):
     with open(f"speaking_{part}_formatted.json", "r", encoding="utf-8") as f:
         data = json.load(f)
         return data, 200
-@app.route("/translate/<source>/<target>/<text>")
-def translate_user(source, target, text):
-    return translator.translate_text(text, source_lang=source, target_lang=target)
+@app.route("/translate")
+def translate_user():
+    source = request.args.get("source")
+    target = request.args.get("target")
+    text = request.args.get("text")
+    res = {"text": translator.translate_text(source_lang=source, target_lang=target, text=text)}
+    return res, 200
 
 
 if __name__ == "__main__":
